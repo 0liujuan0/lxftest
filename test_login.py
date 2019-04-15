25701aa9df237949b8f86aab7c2ee35afc7d3d38
duobi <duobi@duobideMacBook-Pro.local>
2019-04-15 10:15:31 +0800

    use xpaths.py to manage xpaths

--- test_login.py
+++ test_login.py
@@ -14,20 +14,20 @@ class TestLogin(unittest.TestCase):
 
     def test_login_success(self):
         self.driver.get(conf.login_url)
-        name = self.driver.find_element_by_xpath('//*[@id="username"]')
+        name = self.driver.find_element_by_xpath(xpaths.login_username)
         name.send_keys(conf.username)
-        password = self.driver.find_element_by_xpath('//*[@id="pwd"]')
+        password = self.driver.find_element_by_xpath(xpaths.login_password)
         password.send_keys(conf.password)
-        login = self.driver.find_element_by_xpath('//*[@id="loginform"]/button')
+        login = self.driver.find_element_by_xpath(xpaths.login_btn)
         login.click()
         self.assertEqual(self.driver.title, u'东阳大数据平台', msg=u'login failed with correct password')
 
     def test_login_wrong_password(self):
         self.driver.get(conf.login_url)
-        name = self.driver.find_element_by_xpath('//*[@id="username"]')
+        name = self.driver.find_element_by_xpath(xpaths.login_username)
         name.send_keys(conf.username)
-        password = self.driver.find_element_by_xpath('//*[@id="pwd"]')
+        password = self.driver.find_element_by_xpath(xpaths.login_password)
         password.send_keys("some_random_chars")
-        login = self.driver.find_element_by_xpath('//*[@id="loginform"]/button')
+        login = self.driver.find_element_by_xpath(xpaths.login_btn)
         login.click()
         self.assertEqual(self.driver.current_url, conf.login_url, msg=u'login success with wrong password')
